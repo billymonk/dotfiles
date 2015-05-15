7eac334d95821d8d7658b6294eaf975e1eb0a29f
Billy Monk <billy.monk@gmail.com>
2015-05-15 07:00:29 -0400

    Add a function to remove vim swp files.

--- functions.sh
+++ functions.sh
@@ -9,3 +9,12 @@ function unbak() {
     cp "${i}~" "${i}"
   done
 }
+
+function remove-vim-swap-files() {
+  for i in $(find . -name \*.sw*); do
+    if [ $(file $i | awk '{print $3}') != "Flash" ];
+    then
+      rm $i
+    fi
+  done
+}
